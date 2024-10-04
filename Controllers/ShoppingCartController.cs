9cd4280ac78d65ce7490e5a223a7d1f7fe800388
Ahmed Ramadan <ahmedrz422@gmail.com>
2024-10-05 01:28:56 +0300

    Enhance AddtoCart and Purchase in Controller

--- Controllers/ShoppingCartController.cs
+++ Controllers/ShoppingCartController.cs
@@ -9,7 +9,7 @@ namespace ECommerce.Controllers
     public class ShoppingCartController : Controller
     {
         private readonly storeContext _db;
-        private  List<ShoppingCartItem> _items;
+        private List<ShoppingCartItem> _items;
         private readonly UserManager<User> _userManager;
         public ShoppingCartController(storeContext context, UserManager<User> userManager)
         {
@@ -17,25 +17,30 @@ namespace ECommerce.Controllers
             _items = new List<ShoppingCartItem>();
             _userManager = userManager;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
         [HttpGet]
         public IActionResult AddtoCart(int id)
         {
-            var _product=_db.Products.Find(id);
-            
-            var _cartItems=HttpContext.Session.Get<List<ShoppingCartItem>>("Cart")??new List<ShoppingCartItem>();
+            var _product = _db.Products.Find(id);
+            var _cartItems = HttpContext.Session.Get<List<ShoppingCartItem>>("Cart") ?? new List<ShoppingCartItem>();
             var extingproduct = _cartItems.FirstOrDefault(x => x.product.Id == id);
-            if (_product.QuantityInStock < extingproduct.Quantitiy || _product.IsActive==false)
+            if (_product.QuantityInStock <= 0 || _product.IsActive == false)
             {
-                
-                return RedirectToAction("Details", "Product", id);
+                return RedirectToAction("Details", "Product", new
+                {
+                    ID = id
+                });
             }
             if (extingproduct != null)
             {
-                extingproduct.Quantitiy++;
+                if (_product.QuantityInStock > extingproduct.Quantitiy)
+                { extingproduct.Quantitiy++; }
+                else
+                {
+                    return RedirectToAction("Details", "Product", new
+                    {
+                        ID = id
+                    });
+                }
             }
             else
             {
@@ -72,13 +77,17 @@ namespace ECommerce.Controllers
 
             return RedirectToAction("ViewCart");
         }
-        
+
         public async Task<IActionResult> Purchase()
         {
             var _cartItems = HttpContext.Session.Get<List<ShoppingCartItem>>("Cart") ?? new List<ShoppingCartItem>();
+            if (_cartItems.Count == 0)
+            {
+                return RedirectToAction("Index", "Product");
+            }
             var orderitemincart = new List<OrderItem>();
             decimal sumtotal = 0;
-            
+
             foreach (var item in _cartItems)
             {
                 var product = _db.Products.Find(item.product.Id);
@@ -86,24 +95,24 @@ namespace ECommerce.Controllers
                 orderitemincart.Add(new OrderItem
                 {
                     Id = item.Id,
-                    ProductId=item.product.Id,
-                    Quantity=item.Quantitiy,
+                    ProductId = item.product.Id,
+                    Quantity = item.Quantitiy,
                     //OrderId=1,
-                    UnitPrice=Convert.ToDecimal(item.product.Price*item.Quantitiy)
+                    UnitPrice = Convert.ToDecimal(item.product.Price * item.Quantitiy)
                 });
-                sumtotal =sumtotal + Convert.ToDecimal(item.product.Price * item.Quantitiy);
+                sumtotal = sumtotal + Convert.ToDecimal(item.product.Price * item.Quantitiy);
                 _db.OrderItems.AddRange(orderitemincart);
             }
             var totals = new Order();
             var user = await _userManager.GetUserAsync(User);
             totals.UserId = user.Id;
             totals.OrderDate = DateTime.Now;
-            totals.OrderItems= orderitemincart;
-            totals.TotalAmount=sumtotal;
+            totals.OrderItems = orderitemincart;
+            totals.TotalAmount = sumtotal;
             _db.Orders.Add(totals);
             _db.SaveChanges();
             HttpContext.Session.Set("Cart", new List<ShoppingCartItem>());
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
