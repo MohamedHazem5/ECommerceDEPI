36dec44ab5eaa24ef84965d31cdf07219403f5d2
AlHusseinHamdy <alhusseinhamdy95@gmail.com>
2024-10-06 00:29:54 +0300

    Update authorize

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -13,7 +13,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace ECommerce.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin,Customer")]
 
     public class CategoryController : Controller
     {
